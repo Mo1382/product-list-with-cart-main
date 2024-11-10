b4ad1b5f45ff756baf462bc24eebd03b01ed744d
Mo1382 <mahdimms.expert999@gmail.com>
2024-11-10 21:50:53 +0330

    readmeme.md file completed

--- README.md
+++ README.md
@@ -34,8 +34,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://github.com/Mo1382/product-list-with-cart-main)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [Link](https://github.com/Mo1382/product-list-with-cart-main)
+- Live Site URL: [Link](https://mo1382.github.io/product-list-with-cart-main/)
 
 ## My process
 
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -80,7 +80,7 @@ const cardRender = function (data, cardsWrapperEl) {
   data.forEach((item) => {
     const cardHTML = `<div class="card px-0 col-12 col-sm-6 offset-sm-0 col-md-6 col-lg-4">
             <div class="card-header">
-              <img class="img-fluid" src=${item.image[userDevice]} alt="" />
+              <img class="img-fluid" src=src${item.image[userDevice]} alt="" />
               <button class="add-to-cart-btn add-one-to-cart-btn">
                 <div class="add-to-cart-icon">
                   <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z" /></clipPath></defs></svg>
@@ -495,7 +495,7 @@ const renderConfiremedItems = function (
 <div class="item">
   <div class="item-detail">
     <div class="item-thumb">
-      <img class="img-fluid" src="${item.image.thumbnail}" alt="">
+      <img class="img-fluid" src="src${item.image.thumbnail}" alt="">
     </div>
     <div class="item-description">
       <h5 class="item-name">${item.name}</h5>
